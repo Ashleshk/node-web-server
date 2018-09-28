6e55cd701cfd0501da717c7423c20d72f040786f
Ashlesh <ashleshuk@gmail.com>
2018-09-28 12:51:03 +0530

    Added projects page and changed views of other pages

--- server.js
+++ server.js
@@ -76,6 +76,12 @@ app.get('/about',(req,res) =>{
     });
 });
 
+app.get('/projects',(req,res) =>{
+    res.render('projects.hbs',{
+        pageTitle:'Projects page' 
+    });
+});
+
 app.get('/bad',(req,res) =>{
     res.send({
         error:'could not load page'
